ee6de489fc6a294685d1ad26e90ffe6c6a07bfad
Dorian Setnik <dorian.setnik9@gmail.com>
2026-01-04 13:28:05 +0100

    Added constants file

--- src/sine-wave-prediction.py
+++ src/sine-wave-prediction.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import constants as c
 
 
 np.random.seed(0)
 
-n_samples = 200
-x_samples = np.random.uniform(low = 0.0, high = 2 * np.pi, size = (n_samples, 1))
-y_samples = np.sin(x_samples) + np.random.normal(loc = 0.0, scale = 0.3, size = (n_samples, 1))
+
+x_samples = np.random.uniform(low = 0.0, high = 2 * np.pi, size = (c.N_SAMPLES, 1))
+y_samples = np.sin(x_samples) + np.random.normal(loc = 0.0, scale = 0.3, size = (c.N_SAMPLES, 1))
 
 plt.scatter(x_samples, y_samples)
 plt.show()
