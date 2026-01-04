d7f4d47c977230bfacd652a9d441b4638102c8dc
Dorian Setnik <dorian.setnik9@gmail.com>
2026-01-04 22:49:24 +0100

    test weight initialization with xavier glorot uniform

--- src/layer.py
+++ src/layer.py
@@ -3,9 +3,10 @@ import numpy as np
 class Layer:
     
     def __init__(self, n_inputs, n_neurons):
-        self.initialize_weights(n_inputs, n_neurons)
+        self.weights = self.init_weights_xavier_glorot_uniform(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
 
-    def initialize_weights(self, fan_in, fan_out):
+    def init_weights_xavier_glorot_uniform(self, fan_in, fan_out):
         
         # Xavier Glorot uniform initialization
         limit = np.sqrt(6 / (fan_in + fan_out))
@@ -16,7 +17,7 @@ class Layer:
             size = (fan_in, fan_out)
         )
 
-        self.weights = weights
+        return weights
 
 
 
--- src/sine-wave-prediction.py
+++ src/sine-wave-prediction.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import constants as c
+from layer import Layer
 
 
 np.random.seed(0)
@@ -9,5 +10,9 @@ np.random.seed(0)
 x_samples = np.random.uniform(low = 0.0, high = 2 * np.pi, size = (c.N_SAMPLES, 1))
 y_samples = np.sin(x_samples) + np.random.normal(loc = 0.0, scale = 0.3, size = (c.N_SAMPLES, 1))
 
-plt.scatter(x_samples, y_samples)
-plt.show()
+#plt.scatter(x_samples, y_samples)
+#plt.show()
+
+layer1 = Layer(c.N_SAMPLES, 2)
+
+print(layer1.weights)
