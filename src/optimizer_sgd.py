a30104725c30d37c7e12fd7cef1a9b6aa2caa3a6
Dorian Setnik <dorian.setnik9@gmail.com>
2026-01-16 09:29:11 +0100

    SGD optimizer class

---
+++ src/optimizer_sgd.py
@@ -0,0 +1,10 @@
+class Optimizer_SGD:
+
+    def __init__(self, learning_rate):
+        self.learning_rate = learning_rate
+
+    def update_parameters(self, layer):
+
+        # update weights by subtracting the negative gradients multiplied with learning rate
+        layer.weights += -self.learning_rate * layer.dweights 
+        layer.biases += -self.learning_rate * layer.dbiases
