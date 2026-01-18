9fbbd18314995ea94baad5f3d1d7fb10c2efbda4
Dorian Setnik <dorian.setnik9@gmail.com>
2026-01-18 14:27:48 +0100

    RMSProp optimization

--- src/sine-wave-prediction.py
+++ src/sine-wave-prediction.py
@@ -9,6 +9,7 @@ from optimizer_sgd import Optimizer_SGD
 from optimizer_sgd_decay import Optimizer_SGD_Decay
 from optimizer_sgd_momentum import Optimizer_SGD_Momentum
 from optimizer_adagrad import Optimizer_Adagrad
+from optimizer_rmsprop import Optimizer_RMSProp
 
 np.random.seed(0)
 
@@ -19,7 +20,8 @@ y_samples = np.sin(x_samples) + np.random.normal(loc = 0.0, scale = 0.3, size = 
 optimizer_sgd = Optimizer_SGD(c.LEARNING_RATE)
 optimizer_sgd_decay = Optimizer_SGD_Decay(c.LEARNING_RATE, c.STEP, c.LEARNING_RATE_DECAY)
 optimizer_sgd_mometum = Optimizer_SGD_Momentum(c.LEARNING_RATE, c.MOMENTUM_BETA)
-optimizer_adagrad = Optimizer_Adagrad(c.LEARNING_RATE, c.ADAGRAD_EPSILON)
+optimizer_adagrad = Optimizer_Adagrad(c.LEARNING_RATE, c.EPSILON)
+optimizer_rmsprop = Optimizer_RMSProp(c.LEARNING_RATE, c.RHO, c.EPSILON)
 
 layer1 = Layer(c.N_FEATURES, c.N_NEURONS_L1)
 layer2 = Layer(c.N_NEURONS_L1, c.N_NEURONS_L2)
@@ -65,10 +67,10 @@ for i in range(c.N_EPOCHS):
     activation1.backward(activation2.error_signal, layer2.weights)
     layer1.backward(activation1.error_signal)
 
-    optimizer_sgd_mometum.update_parameters(layer1)
-    optimizer_sgd_mometum.update_parameters(layer2)
-    optimizer_sgd_mometum.update_parameters(layer3)
-    optimizer_sgd_mometum.update_parameters(layer_output)
+    optimizer_rmsprop.update_parameters(layer1)
+    optimizer_rmsprop.update_parameters(layer2)
+    optimizer_rmsprop.update_parameters(layer3)
+    optimizer_rmsprop.update_parameters(layer_output)
 
 
 plt.scatter(x_samples, y_samples)
