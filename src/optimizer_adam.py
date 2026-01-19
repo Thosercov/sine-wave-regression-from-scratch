Fixed SGD with momentum formula

src/optimizer_adam.py
@@ -17,7 +17,7 @@ class Optimizer_Adam:
             layer.cache_weights = np.zeros(layer.weights.shape)
             layer.cache_biases = np.zeros(layer.biases.shape)
 
-        layer.weight_momentums = self.beta_1 * layer.weight_momentums +  (1 - self.beta_1) * layer.dweights
+        layer.weight_momentums = self.beta_1 * layer.weight_momentums + (1 - self.beta_1) * layer.dweights
         layer.bias_momentums = self.beta_1 * layer.bias_momentums + (1 - self.beta_1) * layer.dbiases
 
         weight_momentums_corrected = layer.weight_momentums / (1 - self.beta_1 ** (self.iterations + 1))

src/optimizer_sgd_momentum.py
@@ -8,17 +8,17 @@ class Optimizer_SGD_Momentum:
     def update_parameters(self, layer):
 
         #1st pass take zeroes
-        if not hasattr(layer, 'dweights_prev'):
-            layer.dweights_prev = np.zeros(layer.weights.shape)
-            layer.dbiases_prev = np.zeros(layer.biases.shape)
+        if not hasattr(layer, 'weight_momentums'):
+            layer.weight_momentums = np.zeros(layer.weights.shape)
+            layer.bias_momentums = np.zeros(layer.biases.shape)
 
-        velocity_weights = self.beta * layer.dweights_prev + self.learning_rate * layer.dweights
-        velocity_biases = self.beta * layer.dbiases_prev + self.learning_rate * layer.dbiases
+        velocity_weights = self.beta * layer.weight_momentums - self.learning_rate * layer.dweights
+        velocity_biases = self.beta * layer.bias_momentums - self.learning_rate * layer.dbiases
 
         # update weights by subtracting the negative gradients multiplied with learning rate
-        layer.weights += -velocity_weights
-        layer.biases += -velocity_biases
+        layer.weights += velocity_weights
+        layer.biases += velocity_biases
 
         #set current biases as previous for the next pass
-        layer.dweights_prev = layer.dweights
-        layer.dbiases_prev = layer.dbiases
+        layer.weight_momentums = velocity_weights
+        layer.bias_momentums = velocity_biases

src/sine-wave-prediction.py
@@ -20,7 +20,7 @@ y_samples = np.sin(x_samples) + np.random.normal(loc = 0.0, scale = 0.3, size = 
 
 optimizer_sgd = Optimizer_SGD(c.LEARNING_RATE)
 optimizer_sgd_decay = Optimizer_SGD_Decay(c.LEARNING_RATE, c.STEP, c.LEARNING_RATE_DECAY)
-optimizer_sgd_mometum = Optimizer_SGD_Momentum(c.LEARNING_RATE, c.MOMENTUM_BETA)
+optimizer_sgd_momentum = Optimizer_SGD_Momentum(c.LEARNING_RATE, c.MOMENTUM_BETA)
 optimizer_adagrad = Optimizer_Adagrad(c.LEARNING_RATE, c.EPSILON)
 optimizer_rmsprop = Optimizer_RMSProp(c.LEARNING_RATE, c.RHO, c.EPSILON)
 optimizer_adam = Optimizer_Adam(c.LEARNING_RATE, c.ADAM_BETA_1, c.ADAM_BETA_2, c.EPSILON)
@@ -69,10 +69,10 @@ for i in range(c.N_EPOCHS):
     activation1.backward(activation2.error_signal, layer2.weights)
     layer1.backward(activation1.error_signal)
 
-    optimizer_adam.update_parameters(layer1)
-    optimizer_adam.update_parameters(layer2)
-    optimizer_adam.update_parameters(layer3)
-    optimizer_adam.update_parameters(layer_output)
+    optimizer_sgd_momentum.update_parameters(layer1)
+    optimizer_sgd_momentum.update_parameters(layer2)
+    optimizer_sgd_momentum.update_parameters(layer3)
+    optimizer_sgd_momentum.update_parameters(layer_output)
 
 
 plt.scatter(x_samples, y_samples)